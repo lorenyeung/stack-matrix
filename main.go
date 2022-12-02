a7339c09773a74b1819c3470289a102c9f24d508
Loren Yeung <loren.w.yeung@gmail.com>
2022-12-01 20:33:44 -0800

    brute force

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 
 	"github.com/julienroland/usg"
@@ -16,11 +17,12 @@ func main() {
 	start := time.Now()
 	//take in input or read in default
 	size := 5
-	m := Matrix{{1, 1, 1, -1, 1},
+	m := Matrix{
+		{2, -10, 1, -1, 1},
 		{-1, 1, 1, -1, -1},
 		{1, 1, 1, -1, 1},
-		{-1, 1, 1, 1, 1},
-		{1, 1, -1, 1, 1}}
+		{-1, -100, -101, -110, -101},
+		{1, 1, -1, 2, 1}}
 	printProblem(size, m)
 	findPath(size, m)
 	duration := time.Since(start)
@@ -28,29 +30,24 @@ func main() {
 }
 
 func findPath(size int, m Matrix) {
+
 	//right down left up
-	heading := Matrix{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+	var sum = m[0][0]
+	heading := Matrix{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
 
 	stack := stack.New()
-	stack.Push([]int{0, 0})
+	//x,y,sum at point in time,value, direction
+	stack.Push([]int{0, 0, m[0][0], m[0][0], 0})
+	log.Info("path[x]:", 0, 0, " pos:", 0, 0, " sum:", sum)
 
 	for {
 		if stack.Len() == 0 {
-			break
+			stack.Push([]int{0, 0, m[0][0], m[0][0], 0})
 		}
 		current := stack.Peek()
-		stack.Pop()
 		currentAssert := current.([]int)
 
-		//mark current position as blocked to prevent backtracking
-		m[currentAssert[0]][currentAssert[1]] = -1
-
-		// base case: reached bottom right, can exit
-		if currentAssert[0] == size-1 && currentAssert[1] == size-1 {
-			log.Info("path found")
-			break
-		}
-
+		log.Debug("attempt new set")
 		for i := 0; i < 4; i++ {
 
 			// Using the direction array
@@ -58,15 +55,57 @@ func findPath(size int, m Matrix) {
 			y := currentAssert[1] + heading[i][1]
 
 			// check next valid move:
-			if x > -1 && y > -1 && x < size && y < size && m[x][y] != -1 {
-				log.Info("path[", printDirection(i), "]:", x, y, " pos:", currentAssert[0], currentAssert[1])
+			if x > -1 && y > -1 && x < size && y < size {
+				//potential answer
 				if x >= size-1 && y >= size-1 {
-					log.Info("path found")
-					return
+					currTop := stack.Peek()
+					currSum := currTop.([]int)
+					if currSum[2] < 0 {
+						log.Warn("sum is less than zero, find another path")
+
+						//pop random lol
+						rand.Seed(time.Now().UnixNano())
+						min := 1
+						max := stack.Len() - 1
+						k := rand.Intn(max-min+1) + min
+
+						for j := 0; j < k; j++ {
+							stack.Pop()
+						}
+						//reset track
+						top := stack.Peek()
+						current := top.([]int)
+
+						//pick new direction
+						rand.Seed(time.Now().UnixNano())
+						min = 0
+						max = 3
+						i = rand.Intn(max-min+1) + min
+						sum = current[2]
+						currentAssert[0] = current[0]
+						currentAssert[1] = current[1]
+						continue
+					} else {
+						log.Info("path found:", sum)
+						for {
+							if stack.Len() == 0 {
+								return
+							}
+							top := stack.Peek()
+							topData := top.([]int)
+							log.Info(topData)
+							stack.Pop()
+						}
+					}
+				} else {
+					sum = sum + m[x][y]
+					log.Info("path[", printDirection(i), "]:", x, y, " pos:", currentAssert[0], currentAssert[1], " sum:", sum, " size:", stack.Len())
+					stack.Push([]int{x, y, sum, m[x][y], i})
+					break
 				}
-				stack.Push([]int{x, y})
 			} else {
-				log.Debug("path[", printDirection(i), "] invalid:", x, y, " pos:", currentAssert[0], currentAssert[1])
+				//stack.Pop()
+				log.Debug("pathInv[", printDirection(i), "]:", x, y, " pos:", currentAssert[0], currentAssert[1], " sum:", sum)
 			}
 		}
 	}
@@ -82,7 +121,7 @@ func printDirection(dir int) string {
 	case 2:
 		return usg.Get.ArrowLeft
 	case 3:
-		return usg.Get.ArrowRight
+		return usg.Get.ArrowUp
 	}
 	return ""
 }
